cd6240ab201e92c0b333a54716ee06cb00ffd7fb
Tom Wilkinson <twilkinson@expedia.com>
2016-11-11 12:01:09 +0000

    Final commit of lesson 8 material

---
+++ Python Level 2/Lesson 8/completed_exercise/restaurants_webserver.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+
+import web
+import shared
+
+urls = ( '/restaurants', 'Restaurants',
+         '/restaurant/(.*)', 'SpecificRestaurant',
+         '/tripdeviser/(.*)', 'TripDeviser',
+         '/addRestaurant','AddRestaurant',
+         '/score/([0-5])', 'RestaurantsByScore',
+         '/', 'Index')
+
+class RestaurantsByScore:
+    def GET (self, score):
+        score = int(score)
+        matching_places = []
+        for restaurant_name in restaurants:
+            if int(restaurants[restaurant_name].fave) == score:
+                matching_places.append(restaurant_name)
+            else:
+                print '{0} has score {1}'.format(restaurant_name, restaurants[restaurant_name].fave)
+        return '\n'.join(sorted(matching_places))
+
+class TripDeviser:
+    def GET (self, place):
+        return generate_response(place)
+
+
+class AddRestaurant:
+    def GET (self):
+        with open("tripdeviser-add.html") as html_template:
+            output_html = html_template.read()
+        output_html = output_html.replace('{{RESTLIST}}', generate_html_rest_list())
+        return output_html
+
+    def POST(self):
+        # Add some code in here to add the input to the internal restaurants data
+        # Then save it out (call the function to save the changes)
+        # Then send the HTML for the new restaurant back, like for TripDevisor above
+        input_data = web.input()
+        name = input_data.name
+        type = input_data.type
+        cost = input_data.cost
+        fave = input_data.fave
+        dist = input_data.dist
+        restaurants[name] = shared.Restaurant(name,type,cost,fave,dist)
+        shared.save_changes(filename,restaurants)
+        return generate_response(name)
+
+
+class Restaurants:
+    def GET(self):
+        print restaurants
+        web.header('Content-Type', 'text/plain')
+        return '\n'.join(sorted(restaurants.keys()))
+
+
+class SpecificRestaurant:
+    def GET (self, name):
+        web.header('Content-Type', 'text/plain')
+        return str(restaurants[name])
+
+
+class Index:
+    def GET(self):
+        return "This is the index page"
+
+def generate_html_rest_list():
+    list_entry = '<li><a href="/tripdeviser/{0}">{0}</a></li>'
+    html_output_fragment  = ''
+    for restaurant_name in sorted(restaurants):
+        html_output_fragment = html_output_fragment + list_entry.format(restaurant_name)
+    return html_output_fragment
+
+
+def generate_response(name):
+    with open("tripdeviser.html") as html_template:
+        output_html = html_template.read()
+    output_html = output_html.replace('{{RESTLIST}}', generate_html_rest_list())
+    output_html = output_html.replace('{{PLACENAME}}', name)
+    output_html = output_html.replace('{{PLACEDESCRIPTION}}', str(restaurants[name]))
+    return output_html
+
+
+filename = 'restaurants-lesson8.csv'
+restaurants = shared.read_csvfile(filename)
+app = web.application(urls, globals())
+
+if __name__ == "__main__":
+    app.run()
---
+++ Python Level 2/Lesson 8/completed_exercise/shared.py
@@ -0,0 +1,78 @@
+import csv
+
+
+def return_verified_value(min, max, value):
+    """Returns an integer between two values.
+    Arguments in order: min, max, value to check"""
+    verified_value = None
+    while verified_value is None:
+        try:
+            verified_value = int(value)
+            if verified_value < min or verified_value > max:
+                verified_value = None
+        except ValueError:
+            verified_value = None
+        if verified_value is None:
+            value = raw_input('Please enter a value between {0} and {1}'.format(min, max))
+    return value
+
+
+def save_changes(filename, restaurants):
+    with open(filename, "wb") as csvfile:
+        csv_writer = csv.DictWriter(csvfile, fieldnames=['name', 'type', 'cost', 'fave', 'dist'])
+        csv_writer.writeheader()
+        for name in restaurants.keys():
+            csv_writer.writerow(restaurants[name].as_dict())
+
+
+def read_csvfile(filename):
+    restaurants = {}
+    with open(filename) as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+        for rest_details in csv_reader:
+            restaurants[rest_details['name']] = Restaurant(**rest_details)
+            # Because the column headers match the Restaurant properties' names, we can use **rest_details as shorthand
+            # We could alo write this as:
+            # Restaurant(rest_details['name'],
+            #            rest_details['type'],
+            #            rest_details['cost'],
+            #            rest_details['fave'],
+            #            rest_details['dist'])
+    return restaurants
+
+
+
+class Restaurant:
+    """Holds details of an individual place to eat"""
+    def __init__(self, name, type, cost, fave, dist):
+        self.name = name
+        self.type = type
+        self.cost = cost
+        self.fave = fave
+        self.dist = dist
+
+    def __str__(self):
+        return "{0} is a {1} place {4} minutes from here. Star rating {2}, Cost {3}".format(
+            self.name, self.type, self.cost, self.fave, self.dist)
+
+    def as_dict(self):
+        return {'type': self.type,
+                'cost': self.cost,
+                'fave': self.fave,
+                'dist': self.dist,
+                'name': self.name}
+
+
+class Formal(Restaurant):
+    """Restaurants which require a reservation and have a dress code"""
+    def make_reservation(self):
+        print "In the future we'll implement this function."
+        print "It'll attempt to make a reservation at {0}".format(self.name)
+
+
+class FastCasual(Restaurant):
+    """Restaurants which don't accept reservations"""
+    def request_delivery(self):
+        print "In the future we'll implement this function."
+        print "It'll request delivery from {0}".format(self.name)
+

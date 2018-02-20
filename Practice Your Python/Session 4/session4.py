76aed2f0ef4c1746c1dbab16b5f157423c7c534b
Tom Wilkinson <twilkinson@expedia.com>
2018-02-20 09:54:14 +0000

    Palindromes!

---
+++ Practice Your Python/Session 4/session4.py
@@ -0,0 +1,2 @@
+def palindrome(sentence):
+    return None
---
+++ Practice Your Python/Session 4/session4_tests.py
@@ -0,0 +1,25 @@
+import unittest
+from session4 import palindrome
+
+
+class TestPalindromeVerification(unittest.TestCase):
+    def test_empty(self):
+        self.assertTrue(palindrome(''))
+
+    def test_oxo(self):
+        self.assertTrue(palindrome('oxo'))
+
+    def test_panama(self):
+        self.assertTrue(palindrome('A Man, A Plan, a Canal: Panama!'))
+
+    def test_elba(self):
+        self.assertTrue(palindrome("Able was I 'ere I saw Elba"))
+
+    def test_seven(self):
+        self.assertFalse(palindrome('seven'))
+
+    def test_nights(self):
+        self.assertTrue(palindrome('1001N? 1001!'))
+
+suite = unittest.TestLoader().loadTestsFromTestCase(TestPalindromeVerification)
+unittest.TextTestRunner(verbosity=2).run(suite)

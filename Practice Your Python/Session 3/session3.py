b24946f9461f5925c3bf7b1906fd89e465060af9
Tom Wilkinson <twilkinson@expedia.com>
2018-10-08 09:51:54 +0100

    Added lookup table from slides, updated vanity url

--- Practice Your Python/Session 3/session3.py
+++ Practice Your Python/Session 3/session3.py
@@ -1,2 +1,16 @@
+lookup_table = {
+    "1": "1",
+    "2": "ABC2",
+    "3": "DEF3",
+    "4": "GHI4",
+    "5": "JKL5",
+    "6": "MNO6",
+    "7": "PQRS7",
+    "8": "TUV8",
+    "9": "WXYZ9",
+    "0": " 0",
+    "": ""
+}
+
 def decode(keypresses):
     return None
